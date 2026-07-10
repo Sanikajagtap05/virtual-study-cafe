c0d466d8c542d0e786e1af223be266899a498e6d
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-07-10 15:56:57 +0530

    Initail React Project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+     
+    
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
